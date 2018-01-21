77181289b7235b49baeb801e4d454c3a4b1d6cf8
ENT8R <info.ent8r@gmail.com>
2018-01-21 21:38:19 +0100

    Add the option to revert an answer

--- api/index.php
+++ api/index.php
@@ -8,7 +8,8 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication(array(
   //Add authentication for POST and DELETE actions
   "path" => [
     "/post",
-    "/delete"
+    "/delete",
+    "/revert"
   ],
   "secure" => true,
   "relaxed" => [
@@ -101,6 +102,44 @@ $app->delete('/delete/{id}', function ($request, $response, $args) {
   } else {
     return $response->withStatus(404)->withJson(["error" => "Could not find data with specified id!"]);
   }
+});
+
+//undo an answer by decreasing the amount
+$app->post('/revert/{id}', function ($request, $response, $args) {
+  $data = file_get_contents('data.json');
+  $file = json_decode($data, true);
+
+  $id = $request->getAttribute('id');
+  $body = $request->getParsedBody();
+
+  if (!$body) {
+    return $response->withStatus(400)->withJson(["error" => "Can't update data! No request body specified!"]);
+  }
+  if (!$id) {
+    return $response->withStatus(400)->withJson(["error" => "No id for data specified!"]);
+  }
+
+  //Loop through values to decrease answer by 1
+  if (isset($body['values'])) {
+    foreach ($body['values'] as $language => $name) {
+      $amount = 0;
+      if (isset($file[0][$id]['values'][$language][$name])) {
+        $amount = $file[0][$id]['values'][$language][$name];
+      }
+      if ($amount != 0 && $amount != 1) {
+        $file[0][$id]['values'][$language][$name] = $amount - 1;
+      } else {
+        unset($file[0][$id]['values']);
+      }
+    }
+  } else {
+    return $response->withStatus(400)->withJson(["error" => "No values found in request body!"]);
+  }
+
+  $newFile = json_encode($file, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+  file_put_contents('data.json', $newFile);
+
+  return $response->withJson($file);
 });
 
 $app->run();
