1eb8fd98ed87472aac67540381c8c33385d7058c
ENT8R <info.ent8r@gmail.com>
2018-01-22 15:41:56 +0100

    Add two new API endpoints for updating and creating a new dataset, rename the post endpoint to answer and update documentation of the API

--- api/index.php
+++ api/index.php
@@ -7,9 +7,10 @@ $app = new Slim\App();
 $app->add(new \Slim\Middleware\HttpBasicAuthentication(array(
   //Add authentication for POST and DELETE actions
   "path" => [
-    "/post",
+    "/answer",
     "/delete",
-    "/revert"
+    "/revert",
+    "/create"
   ],
   "secure" => true,
   "relaxed" => [
@@ -26,17 +27,12 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication(array(
 
 //GET the whole JSON file
 $app->get('/get', function ($request, $response, $args) {
-  $data = file_get_contents('data.json');
-  $json = json_decode($data, true);
-
-  return $response->withJson($json);
+  return $response->withJson(getFile());
 });
 
 //GET data by id
 $app->get('/get/{id}', function ($request, $response, $args) {
-  $data = file_get_contents('data.json');
-  $json = json_decode($data, true);
-
+  $json = getFile();
   $id = $request->getAttribute('id');
 
   if ($json[0][$id]) {
@@ -47,57 +43,46 @@ $app->get('/get/{id}', function ($request, $response, $args) {
 });
 
 //POST a new answer
-$app->post('/post/{id}', function ($request, $response, $args) {
-  $data = file_get_contents('data.json');
-  $file = json_decode($data, true);
-
+$app->post('/answer/{id}', function ($request, $response, $args) {
+  $file = getFile();
   $id = $request->getAttribute('id');
   $body = $request->getParsedBody();
 
-  if (!$body) {
-    return $response->withStatus(400)->withJson(["error" => "Can't update data! No request body specified!"]);
-  }
   if (!$id) {
     return $response->withStatus(400)->withJson(["error" => "No id for data specified!"]);
   }
+  if (!$body) {
+    return $response->withStatus(400)->withJson(["error" => "Can't update data! No request body specified!"]);
+  }
+  if (!isset($body['values'])) {
+    return $response->withStatus(400)->withJson(["error" => "Request body contains wrong or malformed data!"]);
+  }
 
-  //Set new name if value exists in the body of the request
-  if (isset($body['name'])) {
-    $file[0][$id]['name'] = $body['name'];
-  }
-  //Set new type if value exists in the body of the request
-  if (isset($body['type'])) {
-    $file[0][$id]['type'] = $body['type'];
-  }
   //Loop through values to set answer and their amounts
-  if (isset($body['values'])) {
-    foreach ($body['values'] as $language => $name) {
-      $amount = 0;
-      if (isset($file[0][$id]['values'][$language][$name])) {
-        $amount = $file[0][$id]['values'][$language][$name];
-      }
-      $file[0][$id]['values'][$language][$name] = $amount + 1;
+  foreach ($body['values'] as $language => $name) {
+    $amount = 0;
+    if (isset($file[0][$id]['values'][$language][$name])) {
+      $amount = getAmount($file, $id, $language, $name);
     }
+    $file[0][$id]['values'][$language][$name] = $amount + 1;
   }
 
-  $newFile = json_encode($file, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-  file_put_contents('data.json', $newFile);
-
+  saveFile($file);
   return $response->withJson($file);
 });
 
 //DELETE a specific dataset
 $app->delete('/delete/{id}', function ($request, $response, $args) {
-  $data = file_get_contents('data.json');
-  $json = json_decode($data, true);
-
+  $json = getFile();
   $id = $request->getAttribute('id');
+
+  if (!$id) {
+    return $response->withStatus(400)->withJson(["error" => "No id for data specified!"]);
+  }
 
   if ($json[0][$id]) {
     unset($json[0][$id]);
-    $newFile = json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-    file_put_contents('data.json', $newFile);
-
+    saveFile($json);
     return $response->withJson($json);
   } else {
     return $response->withStatus(404)->withJson(["error" => "Could not find data with specified id!"]);
@@ -106,41 +91,113 @@ $app->delete('/delete/{id}', function ($request, $response, $args) {
 
 //undo an answer by decreasing the amount
 $app->post('/revert/{id}', function ($request, $response, $args) {
-  $data = file_get_contents('data.json');
-  $file = json_decode($data, true);
-
+  $file = getFile();
   $id = $request->getAttribute('id');
   $body = $request->getParsedBody();
 
-  if (!$body) {
-    return $response->withStatus(400)->withJson(["error" => "Can't update data! No request body specified!"]);
-  }
   if (!$id) {
     return $response->withStatus(400)->withJson(["error" => "No id for data specified!"]);
   }
+  if (!$body) {
+    return $response->withStatus(400)->withJson(["error" => "Can't update data! No request body specified!"]);
+  }
+  if (!isset($body['values'])) {
+    return $response->withStatus(400)->withJson(["error" => "Request body contains wrong or malformed data!"]);
+  }
 
   //Loop through values to decrease answer by 1
-  if (isset($body['values'])) {
-    foreach ($body['values'] as $language => $name) {
-      $amount = 0;
-      if (isset($file[0][$id]['values'][$language][$name])) {
-        $amount = $file[0][$id]['values'][$language][$name];
-      }
-      if ($amount != 0 && $amount != 1) {
+  foreach ($body['values'] as $language => $name) {
+    if (isset($file[0][$id]['values'][$language][$name])) {
+      $amount = getAmount($file, $id, $language, $name);
+
+      //Delete the child "values" from the file if the amount is smaller than or equal to 1
+      if ($amount > 1) {
         $file[0][$id]['values'][$language][$name] = $amount - 1;
       } else {
         unset($file[0][$id]['values']);
       }
-    }
-  } else {
-    return $response->withStatus(400)->withJson(["error" => "No values found in request body!"]);
-  }
 
-  $newFile = json_encode($file, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-  file_put_contents('data.json', $newFile);
+      saveFile($file);
+    } else {
+      return $response->withStatus(404)->withJson(["error" => "No data found for this language and value!"]);
+    }
+  }
 
   return $response->withJson($file);
 });
+
+//Create a new dataset
+$app->post('/create/{id}', function ($request, $response, $args) {
+  $file = getFile();
+  $id = $request->getAttribute('id');
+  $body = $request->getParsedBody();
+
+  if (!$id) {
+    return $response->withStatus(400)->withJson(["error" => "No id for new dataset specified!"]);
+  }
+  if (!$body) {
+    return $response->withStatus(400)->withJson(["error" => "Can't create new dataset! No request body specified!"]);
+  }
+  if (!isset($body['name']) || !isset($body['type'])) {
+    return $response->withStatus(400)->withJson(["error" => "Request body contains wrong or malformed data!"]);
+  }
+  if (isset($file[0][$id])) {
+    return $response->withStatus(400)->withJson(["error" => "A dataset with this id does already exist!"]);
+  }
+
+  //Set the name of the dataset
+  $file[0][$id]['name'] = $body['name'];
+  //Set the new type
+  $file[0][$id]['type'] = $body['type'];
+
+  saveFile($file);
+
+  return $response->withJson($file);
+});
+
+//Update an existing dataset
+$app->post('/update/{id}', function ($request, $response, $args) {
+  $file = getFile();
+  $id = $request->getAttribute('id');
+  $body = $request->getParsedBody();
+
+  if (!$id) {
+    return $response->withStatus(400)->withJson(["error" => "No id for new dataset specified!"]);
+  }
+  if (!$body) {
+    return $response->withStatus(400)->withJson(["error" => "Can't create new dataset! No request body specified!"]);
+  }
+  if (!isset($file[0][$id])) {
+    return $response->withStatus(400)->withJson(["error" => "A dataset with this id does not exist!"]);
+  }
+
+  if (isset($body['name'])) {
+    //Set the name of the dataset
+    $file[0][$id]['name'] = $body['name'];
+  }
+  if (isset($body['type'])) {
+    //Set the new type
+    $file[0][$id]['type'] = $body['type'];
+  }
+
+  saveFile($file);
+
+  return $response->withJson($file);
+});
+
+function saveFile($file)
+{
+ file_put_contents('data.json', json_encode($file, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
+}
+function getFile()
+{
+  return json_decode(file_get_contents('data.json'), true);
+}
+
+function getAmount($file, $id, $language, $name)
+{
+  return $file[0][$id]['values'][$language][$name];
+}
 
 $app->run();
 
